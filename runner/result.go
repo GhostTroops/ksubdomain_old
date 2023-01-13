500cfebae84545563c5dd4106a4d7239aef1fec9
hktalent <18223385+hktalent@users.noreply.github.com>
2023-01-13 21:18:44 +0800

    优化结果保存为5000条批量提交，减少大数据搜索引擎服务器的压力 2023-01-13

--- runner/result.go
+++ runner/result.go
@@ -5,26 +5,56 @@ import (
 )
 
 // 记录日志到 大数据搜索引擎
-func SendEsLog(m1 map[string]interface{}) {
+func SendEsLog(m1 interface{}) {
 	if 0 == len(util.EsUrl) {
 		return
 	}
-
-	m1["tags"] = "subdomain"
-	szId := util.GetSha1(&m1)
+	szId := "xxx"
 	util.SendReq(&m1, szId, "ksubdomain")
 
 }
 
+var bOk = make(chan struct{})
+var bDo = make(chan struct{})
+var oR = make(chan interface{}, 5000)
+
+func DoSaves() {
+	var n = len(oR)
+	var oS = make([]interface{}, n)
+	for n > 0 {
+		oS = append(oS, <-oR)
+		n--
+	}
+	util.DoSyncFunc(func() {
+		SendEsLog(&oS)
+	})
+}
+
+func DoRunning() {
+	defer DoSaves()
+	for {
+		select {
+		case <-bOk:
+			return
+		case <-bDo:
+			DoSaves()
+		}
+	}
+}
+
 func (r *runner) handleResult() {
+	go DoRunning()
+
 	for result := range r.recver {
-		var m1 = map[string]interface{}{"ip": result.Answers, "subdomain": result.Subdomain}
-		util.DoSyncFunc(func() {
-			SendEsLog(m1)
-		})
+		var m1 = map[string]interface{}{"ip": result.Answers, "subdomain": result.Subdomain, "tags": "subdomain"}
+		oR <- &m1
+		if 5000 <= len(oR) {
+			bDo <- struct{}{}
+		}
 		for _, out := range r.options.Writer {
 			_ = out.WriteDomainResult(result)
 		}
 		r.printStatus()
 	}
+	close(bOk)
 }
