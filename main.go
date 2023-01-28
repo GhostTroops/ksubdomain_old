bc330aa8f1e45d33d20b76cfa09383d8e553fbd3
hktalent <18223385+hktalent@users.noreply.github.com>
2023-01-28 13:50:02 +0800

    init rm ksubdomain.yaml 2023-01-28

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	util "github.com/hktalent/go-utils"
 	myCmd "github.com/hktalent/ksubdomain/cmd/ksubdomain"
+	"os"
 )
 
 func main() {
+	os.RemoveAll("ksubdomain.yaml")
 	util.DoInitAll()
 	myCmd.Main()
 	util.Wg.Wait()
