902eeb8b9e719aba4ba503389af4728669e1da3a
hktalent <18223385+hktalent@users.noreply.github.com>
2023-01-28 12:26:24 +0800

    fixed 无效的0.0.0.1 ip 2023-01-28

--- runner/result.go
+++ runner/result.go
@@ -9,6 +9,9 @@ func (r *runner) handleResult() {
 	defer util.CloseLogBigDb()
 
 	for result := range r.recver {
+		if 1 == len(result.Answers) && "0.0.0.1" == result.Answers[0] {
+			continue
+		}
 		var m1 = map[string]interface{}{"ip": result.Answers, "subdomain": result.Subdomain, "tags": "subdomain"}
 		util.PushLog(&m1)
 		for _, out := range r.options.Writer {
